c2496190507f11e18df04ce2d7c36de1156c8d73
Noah Boos <nboosgamemaker@gmail.com>
2026-01-20 14:22:01 +0100

    Feat. - Changed the root `<View>` to a `ScrollView`> in the `HomeScreen` component.

--- src/features/home/HomeScreen.tsx
+++ src/features/home/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import {Image, View} from "react-native";
+import {Image, ScrollView, View} from "react-native";
 import {Component} from "react";
 import {EventsOfTheYearSection} from "@/src/features/home/sections/EventsOfTheYearSection";
 import {NewsSection} from "@/src/features/home/sections/NewsSection";
@@ -12,14 +12,14 @@ export class HomeScreen extends Component {
 
     render() {
         return (
-            <View>
+            <ScrollView horizontal={false} showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={true}>
                 <View style={tw`w-full h-64 border-transparent rounded-b-lg overflow-hidden`}>
                     <Image style={tw`w-full h-full`} source={{ uri: 'https://picsum.photos/1920/1080' }} resizeMode="cover" />
                 </View>
                 <EventsOfTheYearSection />
                 <NewsSection />
                 <InformationSection />
-            </View>
+            </ScrollView>
         );
     }
 }
