2a94aa175f830d3a7b1bda3f42628a42b6411944
RASAMOELINA ANJANIAINA <anjaniaina.rasamoelina@etu.unistra.fr>
2026-01-24 18:49:17 +0100

    Feat - Added path for the events cards

--- src/components/event/EventCard.tsx
+++ src/components/event/EventCard.tsx
@@ -1,27 +1,30 @@
 import {Component} from "react";
-import {ImageSourcePropType, View, Image, Text } from "react-native";
+import {ImageSourcePropType, View, Image, Text, Pressable } from "react-native";
 import tw from "twrnc";
 
 interface Props {
     source: ImageSourcePropType;
     title: string;
+    onPress?: () => void;
 }
 
 export class EventCard extends Component<Props> {
     render () {
-        const {source, title} = this.props;
+        const {source, title, onPress} = this.props;
 
         return (
-            <View style={tw`flex flex-col size-25 border-transparent rounded-lg overflow-hidden`}>
-                <View style={tw`flex-2`}>
-                    <Image style={tw`w-full h-full`} source={ source } resizeMode="cover" />
+            <Pressable onPress={onPress}>
+                <View style={tw`flex flex-col size-25 border-transparent rounded-lg overflow-hidden`}>
+                    <View style={tw`flex-2`}>
+                        <Image style={tw`w-full h-full`} source={ source } resizeMode="cover" />
+                    </View>
+                    <View style={tw`flex-1 bg-orange-500 justify-center px-2 pt-1 pb-2`}>
+                        <Text style={tw`text-center text-white text-xs`}>
+                            { title }
+                        </Text>
+                    </View>
                 </View>
-                <View style={tw`flex-1 bg-orange-500 justify-center px-2 pt-1 pb-2`}>
-                    <Text style={tw`text-center text-white text-xs`}>
-                        { title }
-                    </Text>
-                </View>
-            </View>
+            </Pressable>
         );
     }
 }
--- src/features/home/sections/EventsOfTheYearSection.tsx
+++ src/features/home/sections/EventsOfTheYearSection.tsx
@@ -2,6 +2,7 @@ import tw from 'twrnc';
 import {View, Text} from "react-native";
 import {Component} from "react";
 import {EventCard} from "@/src/components/event/EventCard";
+import {router} from "expo-router";
 
 export class EventsOfTheYearSection extends Component {
     constructor(props: any) {
@@ -13,9 +14,9 @@ export class EventsOfTheYearSection extends Component {
             <View style={tw`mx-6 my-4`}>
                 <Text style={tw`text-orange-500 font-extrabold uppercase mb-4`}>Évènements à l'année</Text>
                 <View style={tw`flex flex-row justify-between`}>
-                    <EventCard source={{uri: 'https://picsum.photos/200'}} title="Cinéma plein air" />
-                    <EventCard source={{uri: 'https://picsum.photos/200'}} title="Cinéma de quartier" />
-                    <EventCard source={{uri: 'https://picsum.photos/200'}} title="Palmarès" />
+                    <EventCard source={{uri: 'https://picsum.photos/200'}} title="Cinéma plein air" onPress={() => router.push("/(event)/summer_night")} />
+                    <EventCard source={{uri: 'https://picsum.photos/200'}} title="Cinéma de quartier" onPress={() => router.push("/(event)/grindhouse")} />
+                    <EventCard source={{uri: 'https://picsum.photos/200'}} title="Palmarès" onPress={() => router.push("/(event)/winners")} />
                 </View>
             </View>
         );
